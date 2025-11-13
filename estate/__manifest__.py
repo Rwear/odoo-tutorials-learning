8ff27d4db9ce5b095cf328f1c36e17198f5de068
rwear <bliang271@gwu.edu>
2025-11-12 19:35:15 -0800

    Modify the name of estate model

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -1,5 +1,7 @@
 {
-    'name':'estate',
-    'version':'0.1',
-    'depends':['base'],
+    "name": "estate",
+    "version": "0.1",
+    "depends": ["base"],
+    "data": [],              # 之后加 access CSV、views 再填
+    "application": True,     # 让它显示为一个应用
 }
--- estate/models/estate_property.py
+++
@@ -1,5 +0,0 @@
-from odoo import fields, models
-
-class estate_property(models.Model):
-    __name = "estate.property"
-    _description = "Estate Property"
