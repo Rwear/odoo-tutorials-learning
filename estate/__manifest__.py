d0501ddeff28944e5c8bbc45e56b948e4c066b19
rwear <bliang271@gwu.edu>
2025-11-16 23:21:03 -0800

    add Security csv

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -1,7 +1,9 @@
 {
-    "name": "estate",
-    "version": "0.1",
-    "depends": ["base"],
-    "data": [],              # 之后加 access CSV、views 再填
-    "application": True,     # 让它显示为一个应用
+    'name': 'estate',
+    'version': '0.1',
+    'depends': ['base'],
+    'data': [
+        'security/ir.model.access.csv',
+    ],
+    'application': True,
 }
